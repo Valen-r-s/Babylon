d89d15a6fedb56ba784aeb53d801c613f7760c1d
Valentina Restrepo <valentina.re.san@gmail.com>
2023-08-26 10:00:34 -0500

    Change scene size and color

--- fire.js
+++ fire.js
@@ -7,14 +7,14 @@ var createScene = function () {
 
     var directionalLight = new BABYLON.DirectionalLight("directional", new BABYLON.Vector3(0.5, -2.0, 0.0), scene);
 
-    var ground = BABYLON.MeshBuilder.CreatePlane("ground", { size: 500.0 }, scene);
+    var ground = BABYLON.MeshBuilder.CreatePlane("ground", { size: 10 }, scene);
     ground.position = new BABYLON.Vector3(0,0, 0);
     ground.rotation = new BABYLON.Vector3(Math.PI / 2, 0, 0);
 
     var groundMat = new BABYLON.PBRMetallicRoughnessMaterial("groundMat", scene);
-    groundMat.baseColor = new BABYLON.Color4(0.59, 0.98, 0.4);
-    groundMat.metallic = 0;
-    groundMat.roughness = 0.1;
+    groundMat.baseColor = new BABYLON.Color4(0.34, 0.3, 0.1);
+    groundMat.metallic = 1;
+    groundMat.roughness = 3;
     ground.material = groundMat;
     ground.material.backFaceCulling = false;
 
